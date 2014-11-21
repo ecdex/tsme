cc2a880afcbae4b906de9940c0bc662d67e76d52
Glen E. Ivey <gei@mcn.org>
2014-11-21 07:57:29 -0800

    Expanding matrix of browsers used for Sauce Labs integration testing

--- .sauce-configs.js
+++ .sauce-configs.js
@@ -8,14 +8,38 @@
 //  match available Sauce Labs platforms, see https://saucelabs.com/platforms
 
 module.exports = {
-  android: [],
-  chrome: [
-    { platform: "OS X 10.9", version: "" }
+  android: [
+    { platform: "Linux", version: "4.3", deviceOrientation: "portrait",
+      deviceName: "Google Nexus 7C Emulator" }
   ],
-  firefox: [],
-  ie: [],
-  ipad: [],
-  iphone: [],
-  opera: [],
-  safari: []
+  chrome: [
+    { platform: "OS X 10.10", version: "37" },
+    { platform: "OS X 10.9", version: "" },
+    { platform: "OS X 10.8", version: "27" },
+    { platform: "Windows 8.1", version: "35" },
+    { platform: "Linux", version: "35" }
+  ],
+  firefox: [
+    { platform: "OS X 10.10", version: "33" },
+    { platform: "Windows 8.1", version: "33" },
+    { platform: "Linux", version: "33" }
+  ],
+  ie: [
+    { platform: "Windows 8.1", version: "11" },
+    { platform: "Windows 8", version: "10" },
+    { platform: "Windows 7", version: "9" },
+    { platform: "Windows 7", version: "8" }
+  ],
+  iPad: [
+    { platform: "OS X 10.9", version: "8.0", deviceOrientation: "portrait" }
+  ],
+  iPhone: [
+    { platform: "OS X 10.9", version: "8.1", deviceOrientation: "portrait" }
+  ],
+  opera: [
+    { platform: "Windows 7", version: "12" }
+  ],
+  safari: [
+    { platform: "OS X 10.10", version: "8" }
+  ]
 };
