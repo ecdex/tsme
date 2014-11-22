12a298f6221125bc271009e50cd7b807fd8c6f44
Glen E. Ivey <gei@mcn.org>
2014-11-22 08:30:47 -0800

    and Chrome on Mountain Lion

--- .sauce-configs.js
+++ .sauce-configs.js
@@ -20,7 +20,7 @@ module.exports = {
   chrome: [
     { platform: "OS X 10.10", version: "37" },
     { platform: "OS X 10.9", version: "" },
-    //{ platform: "OS X 10.8", version: "27" },    // no browser logs in this version of Chrome?  https://saucelabs.com/tests/1b688ce583cb452196da517973e4cf8f
+    { platform: "OS X 10.8", version: "32" },
     //{ platform: "Linux", version: "35" },        // maybe no Linux browsers are going to work?
     { platform: "Windows 8.1", version: "35" }
   ],
