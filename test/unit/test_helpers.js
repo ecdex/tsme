e6222516f5460d94bf93bb58622190ee6ca60692
Glen E. Ivey <gei@mcn.org>
2015-02-09 11:48:45 -0800

    whitespace only; separate variable definition from assignment to reduce indent level of whole file

--- test/unit/test_helpers.js
+++ test/unit/test_helpers.js
@@ -2,40 +2,41 @@
 
 require("should");
 var sinon = require("sinon"),
+    helpers;
 
-    helpers = {
-      shouldBeAnInstanceOfExpress: function (express) {
-        // first-order duck typing for an express server
-        express.should.be.a.Function;
-        express.length.should.equal(3, "a real express server function has an arity of 3");
-      },
+helpers = {
+  shouldBeAnInstanceOfExpress: function (express) {
+    // first-order duck typing for an express server
+    express.should.be.a.Function;
+    express.length.should.equal(3, "a real express server function has an arity of 3");
+  },
 
-      mockExpressFactory: function (expressStubContainer) {
-        var factory = function () {
-          var appStub = {
-            listen: sinon.stub(),
-            use: sinon.stub(),
-            set: sinon.stub(),
-            engine: sinon.stub(),
-            static: sinon.stub()
-          };
-          expressStubContainer.appStub = appStub;
-          return appStub;
-        };
-
-        factory = sinon.spy(factory);
-        expressStubContainer.expressStub = factory;
-
-        var static = function () {
-          return {
-            spyFunctionName: "static",
-            arguments: arguments
-          };
-        };
-        factory.static = sinon.spy(static);
-
-        return factory;
-      }
+  mockExpressFactory: function (expressStubContainer) {
+    var factory = function () {
+      var appStub = {
+        listen: sinon.stub(),
+        use: sinon.stub(),
+        set: sinon.stub(),
+        engine: sinon.stub(),
+        static: sinon.stub()
+      };
+      expressStubContainer.appStub = appStub;
+      return appStub;
     };
+
+    factory = sinon.spy(factory);
+    expressStubContainer.expressStub = factory;
+
+    var static = function () {
+      return {
+        spyFunctionName: "static",
+        arguments: arguments
+      };
+    };
+    factory.static = sinon.spy(static);
+
+    return factory;
+  }
+};
 
 module.exports = helpers;
